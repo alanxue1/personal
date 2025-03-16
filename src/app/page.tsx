2527f1af424e035441d9b865018ad5805795502f
alanxue1 <130785943+alanxue1@users.noreply.github.com>
2025-03-16 18:18:11 -0400

    feat: enhance footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@ import { AuroraText } from "@/components/magicui/aurora-text";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-[calc(100vh-200px)]">
-      <h1 className="flex items-baseline text-4xl md:text-6xl font-bold">
-        <span className="text-black dark:text-white">Coming</span>
-        <span className="w-6 md:w-5"></span>
-        <div className="transform translate-y-0.5 md:translate-y-0">
-          <AuroraText>
+    <div className="flex items-center justify-center h-[calc(100vh-160px)]">
+      <h1 className="flex flex-wrap items-baseline">
+        <span className="text-black dark:text-white text-[3.5rem] font-bold">Coming</span>
+        <span className="w-5"></span>
+        <div className="transform -translate-y-2.5">
+          <AuroraText className="text-[3.8rem] font-bold">
             Soon
           </AuroraText>
         </div>
